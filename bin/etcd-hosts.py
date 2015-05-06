aa3c3021aef1b0c53ab6ddf5449d519faf0a7a5d
Jim Minter <jminter@redhat.com>
2015-05-06 10:34:53 +0100

    update etcd-hosts

--- bin/etcd-hosts.py
+++ bin/etcd-hosts.py
@@ -9,79 +9,104 @@ import socket
 import tempfile
 
 
+class Hosts(object):
+    def __init__(self):
+        self.h = set()
+
+    def write(self):
+        with open("/etc/hosts", "r") as f:
+            hosts = f.read()
+
+        hosts = re.sub("\n# etcd-hosts begin\n.*# etcd-hosts end\n", "\n",
+                       hosts, flags=re.S)
+
+        f = tempfile.NamedTemporaryFile(dir="/etc", prefix="hosts.",
+                                        delete=False)
+        f.write(hosts)
+        if self.h:
+            f.write("# etcd-hosts begin\n")
+            f.write("".join(["%-13s %s\n" % (ip, h) for h in self.h]))
+            f.write("# etcd-hosts end\n")
+        f.close()
+
+        os.chmod(f.name, 0644)
+        os.rename(f.name, "/etc/hosts")
+
+    def add(self, x):
+        print "create %s" % x
+        self.h.add(x)
+
+    def discard(self, x):
+        print "delete %s" % x
+        self.h.discard(x)
+
+
+class RouteWatcher(object):
+    def __init__(self, host, port, cert, ca):
+        self.url = "https://%s:%s" % (host, port)
+        (self.cert, self.ca) = (cert, ca)
+        self.s = requests.Session()
+        self.get_routes()
+
+    def get(self, url):
+        r = self.s.get(self.url + url, cert=self.cert, verify=self.ca)
+        self.index = int(r.headers["X-Etcd-Index"])
+        return r.json()
+
+    def get_routes(self):
+        j = self.get("/v2/keys/routes?recursive=true")
+
+        if "node" in j:
+            for domain in j["node"]["nodes"]:
+                if "nodes" in domain:
+                    hs = [json.loads(l["value"])["host"] \
+                          for l in domain["nodes"]]
+                    for h in hs:
+                        hosts.add(h)
+
+        hosts.write()
+
+    def watch_routes(self):
+        j = self.get("/v2/keys/routes?wait=true&recursive=true&waitIndex=%u" %
+                     self.index)
+
+        if j["action"] == "create":
+            hosts.add(json.loads(j["node"]["value"])["host"])
+            hosts.write()
+
+        elif j["action"] == "delete":
+            hosts.discard(json.loads(j["prevNode"]["value"])["host"])
+            hosts.write()
+
+        self.index = j["node"]["modifiedIndex"] + 1
+
+
 def parse_args():
     ap = argparse.ArgumentParser()
-    ap.add_argument("host", nargs="?", default="localhost")
+    ap.add_argument("host", nargs="?", default="ose3-master.example.com")
     ap.add_argument("port", nargs="?", default="4001")
     return ap.parse_args()
 
 
-def update_etc_hosts(j):
-    if j["action"] == "create":
-        _update_etc_hosts(j["action"],
-                          json.loads(j["node"]["value"])["host"])
-    elif j["action"] == "delete":
-        _update_etc_hosts(j["action"],
-                          json.loads(j["prevNode"]["value"])["host"])
-
-
-def _update_etc_hosts(action, hostname):
-    print "%s %s" % (action, hostname)
-
-    with open("/etc/hosts", "r") as f:
-        lines = f.read().strip().split("\n")
-
-    rx = re.compile(("^%s\s+%s$" % (ip, hostname)).replace(".", "\."))
-    lines = [l for l in lines if not rx.match(l)]
-
-    if action == "create":
-        lines.append("%-13s %s" % (ip, hostname))
-
-    f = tempfile.NamedTemporaryFile(dir="/etc", prefix="hosts.", delete=False)
-    f.write("\n".join(lines) + "\n")
-    f.close()
-
-    os.chmod(f.name, 0644)
-    os.rename(f.name, "/etc/hosts")
-
-
-def watch(url, f):
-    global index
-
-    while True:
-        try:
-            j = s.get(url + "&waitIndex=%u" % index, cert=cert,
-                      verify=ca).json()
-            f(j)
-            index = j["node"]["modifiedIndex"] + 1
-
-        except KeyboardInterrupt:
-            break
-
-
-def init():
-    global index
-
-    r = s.get("https://%s:%s/v2/keys/routes?recursive=true" %
-              (args.host, args.port), cert=cert, verify=ca)
-    index = int(r.headers["X-Etcd-Index"])
-    j = r.json()
-
-    if "node" in j:
-        for domain in j["node"]["nodes"]:
-            hosts = [json.loads(l["value"])["host"] for l in domain["nodes"]]
-            for host in hosts:
-                _update_etc_hosts("create", host)
-
-if __name__ == "__main__":
-    s = requests.Session()
-    args = parse_args()
+def load_certs():
     root = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
         "/openshift.local.certificates"
     cert = (root + "/master/etcd-client.crt", root + "/master/etcd-client.key")
     ca = root + "/ca/cert.crt"
-    ip = socket.gethostbyname(args.host)
 
-    init()
-    watch("https://%s:%s/v2/keys/routes?wait=true&recursive=true" %
-          (args.host, args.port), update_etc_hosts)
+    return (cert, ca)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    (cert, ca) = load_certs()
+    ip = socket.gethostbyname(args.host)
+    hosts = Hosts()
+    rw = RouteWatcher(args.host, args.port, cert, ca)
+    try:
+        while True:
+            rw.watch_routes()
+    except KeyboardInterrupt:
+        if hosts.h:
+            hosts.h.clear()
+            hosts.write()
